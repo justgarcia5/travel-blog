1aff7e61709f2758a7f71964fead82f8c97878b8
justingarcia <justgarcia5@gmail.com>
2020-10-21 20:59:16 -0700

    finish section

--- app/javascript/components/pages/Landing.js
+++ app/javascript/components/pages/Landing.js
@@ -3,15 +3,17 @@ import React from 'react'
 import LandingBlogs from '../layout/LandingBlogs'
 import LandingAbout from '../layout/LandingAbout'
 
-import Pear from '../../packs/images/woman.jpg'
-import Logo from '../../packs/images/logo2.png'
+import Camping from '../../packs/images/camping.jpg'
+import Logo1 from '../../packs/images/logo.png'
+import Logo2 from '../../packs/images/logo2.png'
 import LandingNationalParks from '../layout/LandingNationalParks'
 
 export default function Landing() {
   return (
     <div>
-      <img className="logo" src={Logo} />
-      <img className="landing-img" src={Pear} />
+      {/* <img className="logo" src={Logo1} /> */}
+      <img className="logo2" src={Logo2} />
+      <img className="landing-img" src={Camping} />
       <LandingBlogs />
       <LandingAbout />
       <LandingNationalParks />
