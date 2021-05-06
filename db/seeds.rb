0af6aef09542e3c8a6a70879744eaad5485b6238
JUSTGARCIA5 <JUSTGARCIA5@GMAIL.COM>
2021-05-05 21:11:22 -0700

    Update ruby v to 3.0.1

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,15 +6,15 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-post = Post.create(
-  title: "Nevada Falls",
-  longitude: 32.3456,
-  latitude: -112.4321,
-  body: '123456789',
-  location: 'Yosemite'
-)
-post.images.attach(
-  io: File.open('app/javascript/packs/images/pearlake.jpg'),
-  filename: 'pearlake.jpg'
-)
-post.save!
+# post = Post.create(
+#   title: "Nevada Falls",
+#   longitude: 32.3456,
+#   latitude: -112.4321,
+#   body: '123456789',
+#   location: 'Yosemite'
+# )
+# post.images.attach(
+#   io: File.open('app/javascript/packs/images/pearlake.jpg'),
+#   filename: 'pearlake.jpg'
+# )
+# post.save!
